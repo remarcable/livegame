b6f420d105b5d5256b6ecfc3880eec35a7a102c3
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-17 12:12:22 +0200

    Check arguments before submitting registration

--- imports/api/users/registration.js
+++ imports/api/users/registration.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { Accounts } from 'meteor/accounts-base';
 
 Meteor.loginWithName = (firstName, lastName, callback) => {
+  check(firstName, String);
+  check(lastName, String);
+
   const loginRequest = {
     firstName,
     lastName,

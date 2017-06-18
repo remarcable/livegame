d941e4fdcff07cde5684bbd55af569693ef6dcdd
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-18 23:33:25 +0200

    Show different views depending on app state

--- imports/ui/components/ContentWrapper/ContentWrapper.js
+++ imports/ui/components/ContentWrapper/ContentWrapper.js
@@ -1,16 +1,49 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { spacing } from 'material-ui/styles';
+import PropTypes from 'prop-types';
+
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+import { spacing } from 'material-ui/styles';
+
+import Games from '../../../api/games/collection';
+import Submissions from '../../../api/submissions/collection';
+
+import LoginPage from '../../Pages/Login';
+import ActiveGamePage from '../../Pages/ActiveGame';
 import WaitingPage from '../../Pages/Waiting';
 
+const propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  gameIsActive: PropTypes.bool.isRequired,
+  userHasSubmittedForCurrentGame: PropTypes.bool.isRequired,
+};
 
-const ContentWrapper = () => (
-  <div style={styles}>
-    <WaitingPage wrapperStyles={wrapperStyles} />
-  </div>
-);
+const ContentWrapper = ({ isLoggedIn, gameIsActive, userHasSubmittedForCurrentGame }) => {
+  if (!isLoggedIn) {
+    return (
+      <div style={styles}>
+        <LoginPage wrapperStyles={wrapperStyles} />
+      </div>
+    );
+  }
+
+  if (gameIsActive && !userHasSubmittedForCurrentGame) {
+    return (
+      <div style={styles}>
+        <ActiveGamePage wrapperStyles={wrapperStyles} />
+      </div>
+    );
+  }
+
+  return (
+    <div style={styles}>
+      <WaitingPage wrapperStyles={wrapperStyles} />
+    </div>
+  );
+};
+
+ContentWrapper.propTypes = propTypes;
 
 const styles = {
   width: '100%',
@@ -27,5 +60,13 @@ const wrapperStyles = {
 export default ContentWrapperContainer = createContainer(() => {
   const gamesHandle = Meteor.subscribe('games.active');
   const submissionsHandle = Meteor.subscribe('submissions.own');
-  return {};
+
+  const userId = Meteor.userId();
+  const currentGame = Games.findOne({ state: 'active' }) || {};
+  const gameId = currentGame._id;
+
+  const isLoggedIn = !!userId;
+  const gameIsActive = !!gameId;
+  const userHasSubmittedForCurrentGame = !!Submissions.findOne({ userId, gameId });
+  return { isLoggedIn, gameIsActive, userHasSubmittedForCurrentGame };
 }, ContentWrapper);

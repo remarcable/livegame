5a21e43e6d3bcb37d09cfccb0df78a486207f5e4
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-19 00:41:36 +0200

    Only publish AppState if user is logged in

--- imports/api/appState/server/publication.js
+++ imports/api/appState/server/publication.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import AppState from '../collection';
 
 Meteor.publish('appState', function () {
+  if (!this.userId) return this.ready();
   return AppState.find({}, {
     fields: {
       gameEnded: 1,

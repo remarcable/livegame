aa1efade0a93f4c3981386049e6e495c15689547
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-18 23:55:14 +0200

    Add ability to login

--- imports/ui/Pages/Login/LoginPage.js
+++ imports/ui/Pages/Login/LoginPage.js
@@ -1,3 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
@@ -10,25 +12,40 @@ const propTypes = {
 };
 
 const LoginPage = ({ wrapperStyles = {} }) => (
-  <div style={{ ...wrapperStyles, ...styles }}>
+  <form style={{ ...wrapperStyles, ...styles }} onSubmit={handleOnSubmit}>
     <TextField
       floatingLabelText="Vorname"
+      name="firstName"
+      autoComplete="given-name"
+      maxLength="10"
       fullWidth
     />
     <TextField
       floatingLabelText="Nachname"
+      name="lastName"
+      autoComplete="family-name"
+      maxLength="20"
       fullWidth
     />
     <RaisedButton
       label="Anmelden"
       secondary
       labelStyle={{ color: '#fff' }}
+      type="submit"
       style={{ marginTop: theme.spacing.desktopGutter }}
     />
-  </div>
+  </form>
 );
 
 LoginPage.propTypes = propTypes;
+
+const handleOnSubmit = (e) => {
+  e.preventDefault();
+  const firstName = e.target.firstName.value;
+  const lastName = e.target.lastName.value;
+
+  Meteor.loginWithName(firstName, lastName);
+};
 
 const styles = {
   width: '100%',

20126e0e5f427629249417577fb65c604b2efbf2
Marc Nitzsche <marc.nitzsche@live.de>
2017-06-19 21:54:21 +0200

    Add react-router-dom and react-loadable

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -6,14 +12,28 @@ import 'normalize.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 
 import muiTheme from './components/theme';
-import MainLayout from './Layouts/MainLayout';
+import LoadingPage from './Pages/Loading';
 
 injectTapEventPlugin();
 
 const App = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
-    <MainLayout />
+    <Router>
+      <Switch>
+        <Route exact path="/admin" component={AdminArea} />
+        <Route exact path="/admin/scoreboard" component={Scoreboard} />
+        <Route component={LoadableComponent} />
+      </Switch>
+    </Router>
   </MuiThemeProvider>
 );
 
 export default App;
+
+const AdminArea = () => <h1>Hello World!</h1>;
+const Scoreboard = () => <h1>Scoreboard</h1>;
+
+const LoadableComponent = Loadable({
+  loader: () => import('../ui/Layouts/MainLayout'),
+  loading: LoadingPage,
+});

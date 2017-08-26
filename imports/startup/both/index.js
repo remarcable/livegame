ce8d993097c515a39617aced6a645cccbb567098
Marc Nitzsche <marc.nitzsche@live.de>
2017-08-27 00:04:36 +0200

    Don't allow account creation from client

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 import './collections';
 import './methods';
+
+Accounts.config({
+  forbidClientAccountCreation: true,
+});
 
 Meteor.userIsAdmin = function userIsAdmin(id) {
   const userId = Meteor.isServer ? id : Meteor.userId();

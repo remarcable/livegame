3acacebcb353b62914a9e6f817aa8f2ac71df142
Marc Nitzsche <marc.nitzsche@live.de>
2018-03-03 23:46:40 +0100

    Remove error checks in helpers to improve performance

--- imports/api/ranking/calculate-points/helpers.test.js
+++ imports/api/ranking/calculate-points/helpers.test.js
@@ -14,15 +14,6 @@ describe('getDeviation(n, m)', () => {
   it('returns correct deviation for floats', () => {
     expect(getDeviation(100.323, 29.8)).toEqual(70.523);
   });
-
-  it('throws if only one value is provided', () => {
-    expect(() => { getDeviation(100); }).toThrow();
-    expect(() => { getDeviation(undefined, 100); }).toThrow();
-  });
-
-  it('throws if any argument is not a number', () => {
-    expect(() => { getDeviation('foo', 'bar'); }).toThrow();
-  });
 });
 
 describe('addItemToMap(key, value, map)', () => {
@@ -45,36 +36,6 @@ describe('addItemToMap(key, value, map)', () => {
     addItemToMap(fakeUserId, fakePoints, myMap);
 
     expect(myMap.get(fakeUserId)).toEqual(10);
-  });
-
-  it('throws if an argument is missing', () => {
-    const fakeUserId = 100;
-    const fakePoints = 5;
-    const myMap = new Map();
-
-    expect(() => addItemToMap()).toThrow();
-    expect(() => addItemToMap(fakeUserId)).toThrow();
-    expect(() => addItemToMap(fakeUserId, fakePoints)).toThrow();
-    expect(() => addItemToMap(undefined, fakePoints, myMap)).toThrow();
-    expect(() => addItemToMap(undefined, undefined, myMap)).toThrow();
-  });
-
-  it('throws if value (second argument) is not a number', () => {
-    const fakeUserId = 100;
-    const fakePoints = 'foo';
-    const myMap = new Map();
-
-    expect(() => addItemToMap(fakeUserId, fakePoints, myMap)).toThrow();
-  });
-
-  it('throws if map (third argument) is not provided', () => {
-    const fakeUserId = 100;
-    const fakePoints = 5;
-    const myObj = {};
-    const myNum = 42;
-
-    expect(() => addItemToMap(fakeUserId, fakePoints, myObj)).toThrow();
-    expect(() => addItemToMap(fakeUserId, fakePoints, myNum)).toThrow();
   });
 });
 
@@ -116,10 +77,5 @@ describe('getNumberOfOccurences(elements)', () => {
     expect(occurences.get('foo')).toEqual(3);
     expect(occurences.get('bar')).toEqual(2);
     expect(occurences.get('baz')).toEqual(1);
-  });
-
-  it('throws if no array is passed', () => {
-    expect(() => { getNumberOfOccurences(); }).toThrow();
-    expect(() => { getNumberOfOccurences('FooBar'); }).toThrow();
   });
 });

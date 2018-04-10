844222f4a224d2548e75a8e518bcd0b2dd923638
Marc Nitzsche <marc.nitzsche@live.de>
2018-04-10 13:35:35 +0200

    Remove api for games, votings and votingSubmissions

--- imports/api/votingSubmissions/server/publication.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import VotingSubmissions from '../collection';
-
-Meteor.publish('votingSubmissions.own', function() {
-  if (!this.userId) return this.ready();
-  return VotingSubmissions.find({ userId: this.userId });
-});

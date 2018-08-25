d510c6aae10a1804f8809e066e89cf35be5329a8
Marc Nitzsche <marc.nitzsche@live.de>
2018-08-25 23:59:07 +0200

    Add drop shadow to logo

--- imports/ui/components/Logo/index.js
+++ imports/ui/components/Logo/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
+
+import classnames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 
 const Logo = ({ classes }) => (
-  <>
+  <div className={classes.wrapper}>
     <img
       className={classes.image}
       src="/img/logo.png"
       alt="Wer besiegt Paul? Logo"
       srcSet="/img/logo.png 1x, /img/logo2x.png 2x"
     />
-  </>
+    <div className={classnames(classes.image, classes.shadow)} />
+  </div>
 );
 
-const styles = {
-  image: {
-    width: 200,
-    height: 200,
+const styles = (theme) => ({
+  wrapper: {
+    position: 'relative',
+    width: 150,
+    height: 150,
+    minWidth: 150,
+    minHeight: 150,
   },
-};
+  image: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  shadow: {
+    zIndex: -1,
+    borderRadius: '50%',
+    boxShadow: theme.shadows[20],
+  },
+});
 
 export default withStyles(styles)(Logo);

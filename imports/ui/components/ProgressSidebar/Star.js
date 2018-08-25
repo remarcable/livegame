1b009c5502b9e37d11efd103ba15d5bafc9123c0
Marc Nitzsche <marc.nitzsche@live.de>
2018-08-25 22:16:22 +0200

    Add ProgressSidebar component

--- imports/ui/components/ProgressSidebar/Star.js
+++ imports/ui/components/ProgressSidebar/Star.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import classnames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 
 const propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
   state: PropTypes.oneOf(['ACTIVE', 'CORRECT', 'WRONG', 'NOT_YET_PLAYED', 'SUBMITTED']).isRequired,
 };
 
-const Star = ({ classes, state }) => <div className={classes.outer}>{state}</div>;
+const Star = ({ classes, state }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    className={classnames(classes[state], classes.base)}
+  >
+    <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+    <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+  </svg>
+);
 
 Star.propTypes = propTypes;
 
 const styles = () => ({
-  outer: {},
+  base: {
+    filter: 'drop-shadow(0px 2px 9px #000)',
+    stroke: 'transparent',
+    strokeWidth: 1.5,
+    transition: 'all .3s',
+    willChange: 'fill, filter, stroke',
+  },
+  ACTIVE: {
+    fill: '#F4CB44',
+    filter: 'drop-shadow(0px 0px 12px #FFF)',
+  },
+  CORRECT: {
+    fill: '#E9B60F',
+  },
+  WRONG: {
+    fill: '#B1B1B1',
+  },
+  NOT_YET_PLAYED: {
+    stroke: '#6C6C6C',
+    fill: 'transparent',
+  },
+  SUBMITTED: {
+    fill: '#7E630B',
+  },
 });
 
 export default withStyles(styles)(Star);
--- imports/ui/components/ProgressSidebar/index.js
+++ imports/ui/components/ProgressSidebar/index.js
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Star from './Star';
 
 const propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
   games: PropTypes.arrayOf(
     PropTypes.shape({
       estimationGame: PropTypes.shape({
@@ -17,18 +17,63 @@ const propTypes = {
   ).isRequired,
 };
 
-const ProgressSidebar = ({ classes, games }) => (
-  <div className={classes.wrapper}>
-    {games.map(({ fullShowGame: { gameNumber }, state }) => (
-      <Star key={gameNumber} state={state} />
-    ))}
-  </div>
-);
+const ProgressSidebar = ({ classes, games }) => {
+  const correctGamesCount = games.filter((g) => g.state === 'CORRECT').length;
+  return (
+    <div className={classes.wrapper}>
+      {games.map(({ fullShowGame: { gameNumber }, state }) => (
+        <Star key={gameNumber} state={state} />
+      ))}
+
+      <div className={classes.scoreText}>
+        <span className={classes.score}>{correctGamesCount}</span>
+        <span className={classes.scoreDescription}>
+          {correctGamesCount === 1 ? 'Punkt' : 'Punkte'}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 ProgressSidebar.propTypes = propTypes;
 
+// TODO: use theme variable
 const styles = () => ({
-  wrapper: {},
+  wrapper: {
+    position: 'relative',
+    width: 50,
+    height: '100%',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    backgroundColor: '#232D33',
+    color: '#ddd',
+
+    textAlign: 'center',
+    textTransform: 'uppercase',
+
+    boxShadow: 'inset -13px 0px 17px -8px rgba(0, 0, 0, .5)',
+  },
+  scoreText: {
+    position: 'absolute',
+    left: 0,
+    bottom: 10,
+
+    width: 50,
+
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  score: {
+    fontFamily: 'GothamBold',
+    fontSize: 25,
+  },
+  scoreDescription: {
+    fontSize: 10,
+  },
 });
 
 export default withStyles(styles)(ProgressSidebar);

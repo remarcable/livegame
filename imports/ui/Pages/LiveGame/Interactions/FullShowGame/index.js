ca8006eee1d0b4ec16034895d76856047962c355
Marc Nitzsche <marc.nitzsche@live.de>
2018-08-31 16:53:53 +0200

    Remove buttons from FullShowGame

--- imports/ui/Pages/LiveGame/Interactions/FullShowGame/index.js
+++ imports/ui/Pages/LiveGame/Interactions/FullShowGame/index.js
@@ -4,23 +4,15 @@ import PropTypes from 'prop-types';
 import FullShowWaiting from '../FullShowWaiting';
 
 const propTypes = {
-  gameNumber: PropTypes.number.isRequired,
   hasSubmitted: PropTypes.bool.isRequired,
-  submit: PropTypes.func.isRequired,
 };
 
-const FullShowGame = ({ gameNumber, hasSubmitted, submit }) => {
+const FullShowGame = ({ hasSubmitted }) => {
   if (hasSubmitted) {
     return <FullShowWaiting />;
   }
 
-  return (
-    <div>
-      <p>GameNumber: {gameNumber}</p>
-      <button onClick={() => submit('CANDIDATE1')}>Kandidat 1</button>
-      <button onClick={() => submit('CANDIDATE2')}>Kandidat 2</button>
-    </div>
-  );
+  return null;
 };
 
 FullShowGame.propTypes = propTypes;

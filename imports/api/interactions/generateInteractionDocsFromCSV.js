e7268359121dfa1cd25f51430f59828d1d115cfd
Marc Nitzsche <marc.nitzsche@live.de>
2019-02-07 14:52:01 +0100

    Improve generateInteractionDocsFromCSV

--- imports/api/interactions/generateInteractionDocsFromCSV.js
+++ imports/api/interactions/generateInteractionDocsFromCSV.js
@@ -1,33 +1,59 @@
 import { Meteor } from 'meteor/meteor';
 
+const makeDoc = ({ interactionType, title, data = {} }) => ({
+  interactionType,
+  title,
+  data,
+});
+
+const makeFullShowGameDoc = ({ interactionType, title, args }) => {
+  const [gameNumber] = args;
+  const data = { gameNumber: +gameNumber };
+  return makeDoc({ interactionType, title, data });
+};
+
+const makeEstimationGameDoc = ({ interactionType, title, args }) => {
+  const [question, answer] = args;
+  const data = { question, answer: +answer };
+  return makeDoc({ interactionType, title, data });
+};
+
+const makeEstimationVotingDoc = ({ interactionType, title, args }) => {
+  const [question] = args;
+  const data = { question };
+  return makeDoc({ interactionType, title, data });
+};
+
+const makeShowBreakDoc = ({ interactionType, title, args }) => {
+  const [template] = args;
+  const data = { template };
+  return makeDoc({ interactionType, title, data });
+};
+
+const DEFAULT_DOCUMENT_MAKER = makeDoc;
+const documentMakers = {
+  SHOW_BREAK: makeShowBreakDoc,
+  FULL_SHOW_GAME: makeFullShowGameDoc,
+  ESTIMATION_GAME: makeEstimationGameDoc,
+  ESTIMATION_VOTING: makeEstimationVotingDoc,
+  ESTIMATION_WAITING: DEFAULT_DOCUMENT_MAKER,
+  FULL_SHOW_WAITING: DEFAULT_DOCUMENT_MAKER,
+};
+
 export default (string) =>
   string
     .split('\n')
     .filter((line) => line !== '')
     .map((line) => {
       const [interactionType, title, ...args] = line.split(', ');
+      const documentMaker = documentMakers[interactionType];
 
-      const doc = { interactionType, title, data: {} };
-      if (interactionType === 'SHOW_BREAK') {
-        const template = args[0];
-        doc.data = { template };
-      } else if (interactionType === 'FULL_SHOW_GAME') {
-        const gameNumber = +args[0];
-        doc.data = { gameNumber };
-      } else if (interactionType === 'ESTIMATION_GAME') {
-        const question = args[0];
-        const answer = +args[1];
-        doc.data = { question, answer };
-      } else if (interactionType === 'ESTIMATION_VOTING') {
-        const question = args[0];
-        doc.data = { question };
-      } else if (interactionType === 'ESTIMATION_WAITING' || 'FULL_SHOW_WAITING') {
-      } else {
+      if (!documentMaker) {
         throw new Meteor.Error(
           'generateInteractionDocs.invalidInteractionType',
           'interactionType is invalid',
         );
       }
 
-      return doc;
+      return documentMaker({ interactionType, title, args });
     });

c65b58b9b3c9d7fef5cf643ab5ed25726ed066be
Marc Nitzsche <marc.nitzsche@live.de>
2019-09-11 19:57:32 +0200

    Add some stylings

--- .fallback-page/src/timer.js
+++ .fallback-page/src/timer.js
@@ -5,10 +5,11 @@ const timer = Timer('2019-09-28T20:00', {
 
 timer.start();
 
+const element = document.getElementById('countdown');
 timer.ticker(({ formattedTime }) => {
-  document.getElementById('app').innerText = formattedTime;
+  element.innerText = formattedTime;
 });
 
 timer.finish((e) => {
-  document.getElementById('app').innerText = 'boom';
+  element.innerText = 'boom';
 });

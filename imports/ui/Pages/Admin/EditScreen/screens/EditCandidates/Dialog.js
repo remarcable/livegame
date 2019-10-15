7743187acfa544692c739944e2288e87df4cc170
Marc Nitzsche <marc.nitzsche@live.de>
2019-10-15 22:47:37 +0200

    Clean up candidate edit screen

---
+++ imports/ui/Pages/Admin/EditScreen/screens/EditCandidates/Dialog.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import SimpleSchema from 'simpl-schema';
+import SimpleSchemaBridge from 'uniforms-bridge-simple-schema-2';
+import AutoForm from 'uniforms-material/AutoForm';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import MuiDialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import { hintText } from './hintText';
+
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  candidateModel: PropTypes.object, // TODO: better type
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
+
+const schema = new SimpleSchema({
+  name: { type: String, label: 'Name' },
+  imageUrl: { type: String, label: 'Bild-URL', regEx: SimpleSchema.RegEx.Url },
+});
+
+const schemaBridge = new SimpleSchemaBridge(schema);
+
+// eslint-disable-next-line import/prefer-default-export
+const Dialog = ({ title, candidateModel = {}, open, handleClose }) => {
+  const classes = useStyles();
+
+  const [formInstance, setForm] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [name, setName] = useState(null);
+
+  const close = (...args) => {
+    handleClose(...args);
+    setImageUrl(null);
+    setName(null);
+  };
+
+  return (
+    <MuiDialog open={open} onClose={() => close()} className={classes.dialog}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <Box display="flex" justifyContent="center" alignItems="center" mb={1}>
+          <Box mr={3}>
+            <Avatar src={imageUrl || candidateModel.imageUrl} className={classes.avatar}>
+              {!imageUrl && name && name[0].toUpperCase()}
+            </Avatar>
+          </Box>
+          <DialogContentText className={classes.hintText}>{hintText}</DialogContentText>
+        </Box>
+        <AutoForm
+          schema={schemaBridge}
+          onSubmit={(data) => {
+            close({ _id: candidateModel._id, ...data });
+          }}
+          model={{ name: candidateModel.name, imageUrl: candidateModel.imageUrl }}
+          ref={(form) => setForm(form)}
+          submitField={() => <input type="submit" hidden />}
+          onChange={(key, value) => {
+            // for automatically updaing the avatar
+            if (key === 'imageUrl') {
+              setImageUrl(value);
+            }
+
+            if (key === 'name') {
+              setName(value);
+            }
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => close()} color="primary">
+          Abbrechen
+        </Button>
+        <Button
+          onClick={() => {
+            if (formInstance) {
+              formInstance.submit();
+            }
+          }}
+          color="primary"
+        >
+          Speichern
+        </Button>
+      </DialogActions>
+    </MuiDialog>
+  );
+};
+
+const useStyles = makeStyles({
+  avatar: {
+    width: 65,
+    height: 65,
+  },
+  list: {
+    margin: 0,
+  },
+  dialog: {
+    minWidth: 400,
+  },
+  hintText: {
+    flexGrow: 1,
+  },
+});
+
+Dialog.propTypes = propTypes;
+
+export default Dialog;
--- imports/ui/Pages/Admin/EditScreen/screens/EditCandidates/index.js
+++ imports/ui/Pages/Admin/EditScreen/screens/EditCandidates/index.js
@@ -4,10 +4,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import SimpleSchema from 'simpl-schema';
-import SimpleSchemaBridge from 'uniforms-bridge-simple-schema-2';
-import AutoForm from 'uniforms-material/AutoForm';
-
 import Candidates from '/imports/api/candidates/collection';
 
 import {
@@ -17,17 +13,8 @@ import {
   setCandidate,
 } from '/imports/api/candidates/methods';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-import Box from '@material-ui/core/Box';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-
 import CandidatesTable from './CandidatesTable';
+import Dialog from './Dialog';
 
 const propTypes = {
   candidates: PropTypes.array.isRequired, // TODO: better type
@@ -35,16 +22,11 @@ const propTypes = {
   modalIsOpened: PropTypes.bool.isRequired,
 };
 
-const schema = new SimpleSchema({
-  name: { type: String, label: 'Name' },
-  imageUrl: { type: String, label: 'Bild-URL', regEx: SimpleSchema.RegEx.Url },
-});
-
-const schemaBridge = new SimpleSchemaBridge(schema);
-
 const callSetCandidate = ({ _id, candidateNumber }) => setCandidate.call({ _id, candidateNumber });
 
 const EditCandidates = ({ candidates, closeModal, modalIsOpened }) => {
+  // have both editDialogId and editDialogIsOpened in state to be able to close the dialog
+  // and still being able to show the edited data while transitioning
   const [editDialogId, setEditDialogId] = useState(null);
   const [editDialogIsOpened, setEditDialogIsOpened] = useState(false);
   const openEditDialog = (id) => {
@@ -59,110 +41,45 @@ const EditCandidates = ({ candidates, closeModal, modalIsOpened }) => {
         candidates={candidates}
         onEditCandidate={(_id) => openEditDialog(_id)}
         onDeleteCandidate={(_id) => {
-          const shouldDelete = confirm('Möchtest du den Kandidaten wirklich löschen?');
+          const shouldDelete = confirm('Soll der Kandidat wirklich gelöscht werden?');
           if (shouldDelete) {
             removeCandidate.call({ _id });
           }
         }}
         setCandidate={callSetCandidate}
       />
-      <CreateDialog open={modalIsOpened} handleClose={closeModal} />
-      <EditDialog
-        candidate={candidates.find(({ _id }) => _id === editDialogId)}
+
+      <Dialog
+        title="Kandidat erstellen"
+        open={modalIsOpened}
+        handleClose={(data) => {
+          closeModal();
+          if (!data) {
+            return;
+          }
+
+          const { name, imageUrl } = data;
+          insertCandidate.call({ name, imageUrl });
+        }}
+      />
+
+      <Dialog
+        title="Kandidat bearbeiten"
+        candidateModel={
+          editDialogId ? candidates.find(({ _id }) => _id === editDialogId) : undefined
+        }
         open={editDialogIsOpened}
-        handleClose={closeEditDialog}
+        handleClose={(data) => {
+          closeEditDialog();
+          if (!data) {
+            return;
+          }
+
+          const { _id, name, imageUrl } = data;
+          updateCandidate.call({ _id, name, imageUrl });
+        }}
       />
     </>
-  );
-};
-
-const CreateDialog = ({ open, handleClose }) => {
-  const [formInstance, setForm] = useState(null);
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Kandidat erstellen</DialogTitle>
-      <DialogContent>
-        <AutoForm
-          schema={schemaBridge}
-          onSubmit={(data) => {
-            insertCandidate.call(data);
-            handleClose();
-          }}
-          ref={(form) => setForm(form)}
-          submitField={() => <input type="submit" hidden />}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Abbrechen
-        </Button>
-        <Button
-          onClick={() => {
-            if (formInstance) {
-              formInstance.submit();
-            }
-          }}
-          color="primary"
-        >
-          Speichern
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
-
-const useEditDialogStyles = makeStyles({
-  avatar: {
-    width: 65,
-    height: 65,
-  },
-});
-
-const EditDialog = ({ candidate = {}, open, handleClose }) => {
-  const [formInstance, setForm] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
-  const classes = useEditDialogStyles();
-  return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Kandidat bearbeiten</DialogTitle>
-      <DialogContent>
-        <Box display="flex" justifyContent="center" mb={1}>
-          <Avatar src={imageUrl || candidate.imageUrl} className={classes.avatar}>
-            {!imageUrl && candidate.name && candidate.name[0].toUpperCase()}
-          </Avatar>
-        </Box>
-        <AutoForm
-          schema={schemaBridge}
-          onSubmit={(data) => {
-            updateCandidate.call({ _id: candidate._id, ...data });
-            handleClose();
-          }}
-          model={{ name: candidate.name, imageUrl: candidate.imageUrl }}
-          ref={(form) => setForm(form)}
-          submitField={() => <input type="submit" hidden />}
-          onChange={(key, value) => {
-            if (key === 'imageUrl') {
-              setImageUrl(value);
-            }
-          }}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Abbrechen
-        </Button>
-        <Button
-          onClick={() => {
-            if (formInstance) {
-              formInstance.submit();
-            }
-          }}
-          color="primary"
-        >
-          Speichern
-        </Button>
-      </DialogActions>
-    </Dialog>
   );
 };
 

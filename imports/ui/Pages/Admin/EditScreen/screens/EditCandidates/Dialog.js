022dc907bbd61419d12d110227602a252386fb4f
Marc Nitzsche <marc.nitzsche@live.de>
2019-10-18 09:24:39 +0200

    Create info dialog for imageUrl

--- imports/ui/Pages/Admin/EditScreen/screens/EditCandidates/Dialog.js
+++ imports/ui/Pages/Admin/EditScreen/screens/EditCandidates/Dialog.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchemaBridge from 'uniforms-bridge-simple-schema-2';
 import AutoForm from 'uniforms-material/AutoForm';
+import AutoField from 'uniforms-material/AutoField';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -13,10 +14,11 @@ import Button from '@material-ui/core/Button';
 import MuiDialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
-import { hintText } from './hintText';
+import ImageUrlInfoDialog from './ImageUrlInfoDialog';
 
 const propTypes = {
   title: PropTypes.string.isRequired,
@@ -40,6 +42,10 @@ const Dialog = ({ title, candidateModel = {}, open, handleClose }) => {
   const [imageUrl, setImageUrl] = useState(null);
   const [name, setName] = useState(null);
 
+  const [imageUrlDialogOpened, setImageUrlDialogOpened] = useState(false);
+  const openImageUrlDialog = () => setImageUrlDialogOpened(true);
+  const closeImageUrlDialog = () => setImageUrlDialogOpened(false);
+
   const close = (...args) => {
     handleClose(...args);
     setImageUrl(null);
@@ -49,53 +55,63 @@ const Dialog = ({ title, candidateModel = {}, open, handleClose }) => {
   const image = imageUrl || candidateModel.imageUrl;
 
   return (
-    <MuiDialog open={open} onClose={() => close()} className={classes.dialog}>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>
-        <Box display="flex" justifyContent="center" alignItems="center" mb={1}>
-          <Box mr={3}>
+    <>
+      <MuiDialog open={open} onClose={() => close()}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <Box display="flex" justifyContent="center" alignItems="center" mb={1}>
             <Avatar src={image} className={classes.avatar}>
               {!image && name && name[0].toUpperCase()}
             </Avatar>
           </Box>
-          <DialogContentText className={classes.hintText}>{hintText}</DialogContentText>
-        </Box>
-        <AutoForm
-          schema={schemaBridge}
-          onSubmit={(data) => {
-            close({ _id: candidateModel._id, ...data });
-          }}
-          model={{ name: candidateModel.name, imageUrl: candidateModel.imageUrl }}
-          ref={(form) => setForm(form)}
-          submitField={() => <input type="submit" hidden />}
-          onChange={(key, value) => {
-            // for automatically updaing the avatar
-            if (key === 'imageUrl') {
-              setImageUrl(value);
-            }
+          <AutoForm
+            schema={schemaBridge}
+            onSubmit={(data) => {
+              close({ _id: candidateModel._id, ...data });
+            }}
+            model={{ name: candidateModel.name, imageUrl: candidateModel.imageUrl }}
+            ref={(form) => setForm(form)}
+            submitField={() => <input type="submit" hidden />}
+            onChange={(key, value) => {
+              // for automatically updaing the avatar
+              if (key === 'imageUrl') {
+                setImageUrl(value);
+              }
 
-            if (key === 'name') {
-              setName(value);
-            }
-          }}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => close()} color="primary">
-          Abbrechen
-        </Button>
-        <Button
-          onClick={() => {
-            if (formInstance) {
-              formInstance.submit();
-            }
-          }}
-          color="primary"
-        >
-          Speichern
-        </Button>
-      </DialogActions>
-    </MuiDialog>
+              if (key === 'name') {
+                setName(value);
+              }
+            }}
+          >
+            <AutoField name="name" />
+            <Box display="flex">
+              <AutoField name="imageUrl" />
+              <Box width={48} display="flex" justifyContent="center" alignItems="center">
+                <IconButton onClick={openImageUrlDialog}>
+                  <InfoIcon />
+                </IconButton>
+              </Box>
+            </Box>
+          </AutoForm>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => close()} color="primary">
+            Abbrechen
+          </Button>
+          <Button
+            onClick={() => {
+              if (formInstance) {
+                formInstance.submit();
+              }
+            }}
+            color="primary"
+          >
+            Speichern
+          </Button>
+        </DialogActions>
+      </MuiDialog>
+      <ImageUrlInfoDialog open={imageUrlDialogOpened} handleClose={closeImageUrlDialog} />
+    </>
   );
 };
 
@@ -107,7 +123,7 @@ const useStyles = makeStyles({
   list: {
     margin: 0,
   },
-  dialog: {
+  dialogContent: {
     minWidth: 400,
   },
   hintText: {

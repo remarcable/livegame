58a4a983db06822434765799917047616b8ef7af
Marc Nitzsche <marc.nitzsche@live.de>
2019-10-20 20:35:46 +0200

    Add sorting to InteractionsTable

--- imports/ui/Pages/Admin/EditScreen/screens/EditInteractions/InteractionsTable.js
+++ imports/ui/Pages/Admin/EditScreen/screens/EditInteractions/InteractionsTable.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { sortableContainer, sortableElement, sortableHandle } from 'react-sortable-hoc';
+
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -11,10 +13,13 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import EditIcon from '@material-ui/icons/Edit';
+import ReorderIcon from '@material-ui/icons/Reorder';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import getTextForInteraction from '/imports/api/helpers/getTextForInteraction';
 import InteractionIcon from '/imports/ui/components/InteractionIcon';
+
+import { moveToPosition } from '/imports/api/interactions/methods';
 
 const propTypes = {
   interactions: PropTypes.array.isRequired, // TODO: better type
@@ -22,38 +27,70 @@ const propTypes = {
   onDeleteInteraction: PropTypes.func.isRequired,
 };
 
-const InteractionsTable = ({ interactions, onEditInteraction, onDeleteInteraction }) => {
+const DragHandle = sortableHandle(() => <ReorderIcon color="disabled" />);
+
+const SortableItem = sortableElement(({ interaction, onEditInteraction, onDeleteInteraction }) => (
+  <TableRow key={interaction._id}>
+    <TableCell padding="checkbox">
+      <Box m={2}>
+        <DragHandle />
+      </Box>
+    </TableCell>
+    <TableCell padding="checkbox">
+      <Box m={2}>
+        <InteractionIcon type={interaction.type} />
+      </Box>
+    </TableCell>
+    <TableCell>{getTextForInteraction(interaction)}</TableCell>
+    <TableCell padding="checkbox">
+      <Tooltip title="Bearbeiten">
+        <IconButton onClick={() => onEditInteraction(interaction._id)}>
+          <EditIcon />
+        </IconButton>
+      </Tooltip>
+    </TableCell>
+    <TableCell padding="checkbox">
+      <Tooltip title="Löschen">
+        <IconButton onClick={() => onDeleteInteraction(interaction._id)}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+    </TableCell>
+  </TableRow>
+));
+
+const SortableContainer = sortableContainer(({ children }) => {
   return (
     <Paper>
       <Table>
-        <TableBody>
-          {interactions.map((interaction) => (
-            <TableRow key={interaction._id}>
-              <TableCell padding="checkbox">
-                <Box m={2}>
-                  <InteractionIcon type={interaction.type} />
-                </Box>
-              </TableCell>
-              <TableCell>{getTextForInteraction(interaction)}</TableCell>
-              <TableCell padding="checkbox">
-                <Tooltip title="Bearbeiten">
-                  <IconButton onClick={() => onEditInteraction(interaction._id)}>
-                    <EditIcon />
-                  </IconButton>
-                </Tooltip>
-              </TableCell>
-              <TableCell padding="checkbox">
-                <Tooltip title="Löschen">
-                  <IconButton onClick={() => onDeleteInteraction(interaction._id)}>
-                    <DeleteIcon />
-                  </IconButton>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        <TableBody>{children}</TableBody>
       </Table>
     </Paper>
+  );
+});
+
+const InteractionsTable = ({ interactions, onEditInteraction, onDeleteInteraction }) => {
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    if (oldIndex === newIndex) {
+      return;
+    }
+
+    const interaction = interactions[oldIndex];
+    moveToPosition.call({ id: interaction._id, pos: newIndex });
+  };
+
+  return (
+    <SortableContainer onSortEnd={onSortEnd} useDragHandle>
+      {interactions.map((interaction, index) => (
+        <SortableItem
+          key={`item-${interaction._id}`}
+          index={index}
+          interaction={interaction}
+          onEditInteraction={onEditInteraction}
+          onDeleteInteraction={onDeleteInteraction}
+        />
+      ))}
+    </SortableContainer>
   );
 };
 

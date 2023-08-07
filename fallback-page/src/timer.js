da9d89996cee3974f36c5b524125f7ec26ef776d
Marc N <git@marcnitzsche.de>
2023-08-08 00:17:49 +0200

    Update fallback page

--- fallback-page/src/timer.js
+++ fallback-page/src/timer.js
@@ -1,5 +1,7 @@
-import Timer from 'timrjs';
-const timer = Timer('2020-09-26T20:00', {
+import { create } from 'timrjs';
+
+const timer = create({
+  startTime: '2023-10-14 19:00',
   formatOutput: 'dd Tage hh Stunden mm Minuten ss Sekunden',
 });
 
